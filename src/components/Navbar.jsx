68ef494e2e62e4b1c89377a8d9328a6534aa8f8e
Gurpreet Singh <gpsingh02414@gmail.com>
2024-05-03 23:00:00 +0530

    code updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Stack } from "@mui/material";
+import logo from "../../public/assets/images/logo.png";
+import { Link } from "react-router-dom";
+import Searchbar from "./Searchbar";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{ position: "sticky", top: 0, justifyContent: "space-between" }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <Searchbar />
+  </Stack>
+);
 
 export default Navbar;

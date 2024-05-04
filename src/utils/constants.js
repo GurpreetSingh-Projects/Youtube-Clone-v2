e21a63fc786bcb147418c8dd80b92811ce7aa1f6
Gurpreet Singh <gpsingh02414@gmail.com>
2024-05-04 23:00:00 +0530

    code updates

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import HomeIcon from "@mui/icons-material/Home";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import CodeIcon from "@mui/icons-material/Code";
 import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
 import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
@@ -13,25 +13,24 @@ import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
 
 export const categories = [
-  { name: "New", icon: <HomeIcon /> },
-  { name: "JS Mastery", icon: <CodeIcon /> },
-  { name: "Coding", icon: <CodeIcon /> },
-  { name: "ReactJS", icon: <CodeIcon /> },
-  { name: "NextJS", icon: <CodeIcon /> },
-  { name: "Music", icon: <MusicNoteIcon /> },
-  { name: "Education", icon: <SchoolIcon /> },
-  { name: "Podcast", icon: <GraphicEqIcon /> },
-  { name: "Movie", icon: <OndemandVideoIcon /> },
-  { name: "Gaming", icon: <SportsEsportsIcon /> },
-  { name: "Live", icon: <LiveTvIcon /> },
-  { name: "Sport", icon: <FitnessCenterIcon /> },
-  { name: "Fashion", icon: <CheckroomIcon /> },
-  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
-  { name: "Comedy", icon: <TheaterComedyIcon /> },
-  { name: "Gym", icon: <FitnessCenterIcon /> },
-  { name: "Crypto", icon: <DeveloperModeIcon /> },
+  { id: 1, name: "New", icon: <HomeIcon /> },
+  { id: 2, name: "JS Mastery", icon: <CodeIcon /> },
+  { id: 3, name: "Coding", icon: <CodeIcon /> },
+  { id: 4, name: "ReactJS", icon: <CodeIcon /> },
+  { id: 5, name: "NextJS", icon: <CodeIcon /> },
+  { id: 6, name: "Music", icon: <MusicNoteIcon /> },
+  { id: 7, name: "Education", icon: <SchoolIcon /> },
+  { id: 8, name: "Podcast", icon: <GraphicEqIcon /> },
+  { id: 9, name: "Movie", icon: <OndemandVideoIcon /> },
+  { id: 10, name: "Gaming", icon: <SportsEsportsIcon /> },
+  { id: 11, name: "Live", icon: <LiveTvIcon /> },
+  { id: 12, name: "Sport", icon: <FitnessCenterIcon /> },
+  { id: 13, name: "Fashion", icon: <CheckroomIcon /> },
+  { id: 14, name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { id: 15, name: "Comedy", icon: <TheaterComedyIcon /> },
+  { id: 16, name: "Gym", icon: <FitnessCenterIcon /> },
+  { id: 17, name: "Crypto", icon: <DeveloperModeIcon /> },
 ];
-
 export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
 export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
 export const demoVideoUrl = "/video/GDa8kZLNhJ4";

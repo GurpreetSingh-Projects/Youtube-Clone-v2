cff9d16ebf01c9a9eaa5b81e4b989d3bd1dfb0c3
Gurpreet Singh <gpsingh02414@gmail.com>
2024-05-25 23:00:00 +0530

    code update

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
+const VideoCard = (props) => {
+  console.log(video);
+  return (
+    <>
+      <Card>
+        {/* <Link to={props.videoId ? `/video/${props.videoId}` : demoVideoUrl}> */}
+          <CardMedia
+            image={props.snippet?.thumbnails?.low?.url}
+            alt={snippet?.title}
+            sx={{ width: 350, height: 200 }}
+          ></CardMedia>
+        </Link>
+      </Card>
+    </>
+  );
 };
 
 export default VideoCard;

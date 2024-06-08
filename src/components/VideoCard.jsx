326675b09160336cdf39e8abff293169aefa531a
Gurpreet Singh <gpsingh02414@gmail.com>
2024-06-08 23:00:00 +0530

    code update

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -10,8 +10,15 @@ import {
   demoChannelTitle,
 } from "../utils/constants";
 export default function VideoCard({ video }) {
+  const description = video.snippet.title.slice(0, 60);
   return (
-    <Card>
+    <Card
+      sx={{
+        width: { md: "320px", xs: "100%" },
+        boxShadow: "none",
+        borderRadius: "10px",
+      }}
+    >
       <Link to={`/video/${video?.id?.videoId}`}>
         <CardMedia
           image={video?.snippet?.thumbnails?.high?.url}
@@ -19,6 +26,31 @@ export default function VideoCard({ video }) {
           sx={{ width: 350, height: 200 }}
         ></CardMedia>
       </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+        <Link to={`/video/${video?.id?.videoId}`}>
+          <Typography variant="subtitle1" fontWeight="bold" color="grey">
+            {/* {video?.snippet?.title?.slice(0, 50) || demoVideoTitle.slice(0, 50)} */}
+            <div dangerouslySetInnerHTML={{ __html: description }} />
+          </Typography>
+        </Link>
+
+        <Link
+          to={
+            video?.snippet?.channelId
+              ? `/channel/${video?.snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography
+            variant="subtitle2"
+            color="white"
+            fontWeight="semibold"
+            m="5px"
+          >
+            {video?.snippet?.channelTitle || demoChannelUrl}
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 }

a7779bf574bf75d14a87dc1313fba944ae008a09
Gurpreet Singh <gpsingh02414@gmail.com>
2024-06-11 23:00:00 +0530

    code update

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Box, CardContent, CardMedia } from "@mui/material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
+const ChannelCard = ({ channelDetail }) => {
+  console.log(channelDetail.snippet.thumbnails.high.url);
+  return (
+    <>
+      <Box sx={{ boxShadow: "none", borderRadius: "20px" }}>
+        <Link to={`channel/${channelDetail?.id?.channelId}/`}>
+          <CardContent
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            <CardMedia
+              image={
+                channelDetail?.snippet?.thumbnails?.high?.url ||
+                demoProfilePicture
+              }
+              alt={channelDetail?.snippet?.title}
+              sx={{ borderRadius: "50%", height: "180px", width: "180px" }}
+            ></CardMedia>
+          </CardContent>
+        </Link>
+      </Box>
+    </>
+  );
 };
 
 export default ChannelCard;

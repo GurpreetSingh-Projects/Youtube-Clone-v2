52b2b3219d54d863543ba4a209216515e6557668
Gurpreet Singh <gpsingh02414@gmail.com>
2024-07-08 23:00:00 +0530

    code update

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { fetchApi } from "../utils/fetchApi";
+import { Box, Card } from "@mui/material";
+import { ChannelCard, Videos } from "./index";
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [channelVideos, setChannelVideos] = useState([]);
+
+  console.log(channelDetail, channelVideos);
+
   const { id } = useParams();
-  return <div>{id}</div>;
+
+  useEffect(() => {
+    fetchApi(`channels?part=snippet&id=${id}`).then((data) => {
+      setChannelDetail(data?.items[0]);
+    });
+
+    fetchApi(`search?channelId=${id}&part=snippet&order=date`).then((data) => {
+      setChannelVideos(data?.items);
+    });
+  }, [id]);
+  return (
+    <>
+      <Box minHeight="95vh">
+        <Box>
+          <div style={{ background: "white", zIndex: 10, height: 200 }}></div>
+        </Box>
+        <ChannelCard
+          channelDetail={channelDetail}
+          marginTop="-100px"
+        ></ChannelCard>
+      </Box>
+      <Box>
+        <Videos videos={channelVideos} />
+      </Box>
+    </>
+  );
 };
 
 export default ChannelDetail;

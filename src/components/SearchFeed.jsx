c98086a0279ed2539e51ea6482f5275e6f53c2b3
Gurpreet Singh <gpsingh02414@gmail.com>
2024-07-14 23:00:00 +0530

    code update

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,5 +1,47 @@
+import { Box, Typography } from "@mui/material";
+import { Sidebar, Videos } from "./index";
+import { fetchApi } from "../utils/fetchApi";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { BorderRight } from "@mui/icons-material";
+
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+  useEffect(() => {
+    fetchApi(`search?part=snippet&q=${searchTerm}`).then((res) => {
+      setVideos(res.items);
+    });
+  }, [searchTerm]);
+
+  return (
+    <>
+      <Box sx={{ display: "flex", flex: "column" }}>
+        <Box style={{ borderRight: "1px solid #3d3d3d" }}>
+          <Sidebar />
+          <Typography
+            className="copyright"
+            variant="body2"
+            sx={{ mt: 1.5, color: "white" }}
+          >
+            Copyright @GurpreetSingh-Projects
+          </Typography>
+        </Box>
+        <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+          <Typography
+            variant="h5"
+            fontWeight="bold"
+            mb={2}
+            sx={{ color: "white" }}
+          >
+            Showing Search results for{" "}
+            <span style={{ color: "#f31503" }}>{searchTerm}</span>
+          </Typography>
+          <Videos videos={videos} />
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default SearchFeed;

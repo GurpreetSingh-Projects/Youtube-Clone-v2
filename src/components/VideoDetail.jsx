9a56376c6caf76aafcb34d3c2dee35a686cce362
Gurpreet Singh <gpsingh02414@gmail.com>
2024-07-29 23:00:00 +0530

    code update

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,5 +1,35 @@
+import { Box } from "@mui/material";
+import { useEffect } from "react";
+import ReactPlayer from "react-player/youtube";
+import { useParams } from "react-router-dom";
+import { fetchApi } from "../utils/fetchApi";
 function VideoDetail() {
-  return <div>VideoDetail</div>;
+  const { id } = useParams();
+  // const [currVid, setCurrVid] = useState("");
+  // console.log(id);
+  // useEffect(
+  //   (e) => {
+  //     fetchApi(
+  //       `videos?part=contentDetails%2Csnippet%2Cstatistics&id=${id}`
+  //     ).then((data) => {
+  //       setCurrVid(data);
+  //     });
+  //   },
+  //   [id]
+  // );
+  return (
+    <Box sx={{ height: "95vh", background: "inherit" }}>
+      <Box>
+        <ReactPlayer
+          className="react-player"
+          url={`https://www.youtube.com/watch?v=${id}`}
+          playing={true}
+          muted
+          controls
+        />
+      </Box>
+    </Box>
+  );
 }
 
 export default VideoDetail;

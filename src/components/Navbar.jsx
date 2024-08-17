ee35f473f1a1ef17a25c9061ccea7d4e5fac8d76
Gurpreet Singh <gpsingh02414@gmail.com>
2024-08-17 23:00:00 +0530

    bug fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,27 @@ import logo from "/assets/images/logo.png";
 import { Link } from "react-router-dom";
 import { Searchbar } from "./index";
 
-const Navbar = () => (
-  <Stack
-    direction="row"
-    alignItems="center"
-    p={2}
-    sx={{
-      position: "sticky",
-      top: 0,
-      justifyContent: "space-between",
-      zIndex: "9999",
-      background: "inherit",
-    }}
-  >
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img className="logoImg" src={logo} alt="logo" />
-    </Link>
-    <Searchbar />
-  </Stack>
-);
+const Navbar = () => {
+  console.log("Hi, Navbar here lol :)");
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        top: 0,
+        justifyContent: "space-between",
+        zIndex: "9999",
+        background: "inherit",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img className="logoImg" src={logo} alt="logo" />
+      </Link>
+      <Searchbar />
+    </Stack>
+  );
+};
 
 export default Navbar;

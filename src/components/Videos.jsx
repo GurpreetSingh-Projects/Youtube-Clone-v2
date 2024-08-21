1ae89080a7acd8a01995dbddded60791c3e4707e
Gurpreet Singh <gpsingh02414@gmail.com>
2024-08-21 10:00:00 +0000

    code update

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,6 +4,8 @@ import { ChannelCard, ChannelDetail, VideoCard } from "./index";
 import { CreateContext } from "../App";
 const Videos = ({ suggested }) => {
   const { videos } = useContext(CreateContext);
+  console.log(videos);
+
   return suggested ? (
     <div className="suggestedvideos">
       {videos.map((item, idx) => (

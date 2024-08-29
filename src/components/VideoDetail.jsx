9aac55d437dbc3ad2420aae56dbea4237862bae6
Gurpreet Singh <gpsingh02414@gmail.com>
2024-08-29 23:00:00 +0530

    improved skeleton loading

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -7,7 +7,9 @@ function VideoDetail() {
   console.log("Hi, videodetail here :)");
   const { id } = useParams();
   const [currVid, setCurrVid] = useState("");
+  const [len, setLen] = useState(430);
   console.log(currVid);
+
   useEffect(() => {
     fetchApi(`videos?part=contentDetails%2Csnippet%2Cstatistics&id=${id}`).then(
       (data) => {
@@ -15,6 +17,19 @@ function VideoDetail() {
       }
     );
   }, [id]);
+  function viewDescription() {
+    if (len == 430) {
+      setLen(currVid.snippet.localized.description.length);
+      document.getElementById("more").style.display = "none";
+      document.getElementById("hide").style.display = "inline";
+    } else setLen(430);
+  }
+  function hideDescription() {
+    setLen(430);
+    document.getElementById("hide").style.display = "none";
+    document.getElementById("more").style.display = "inline";
+  }
+
   return (
     <Box sx={{ background: "inherit", position: "sticky" }}>
       <Stack direction={{ xs: "column", md: "row" }}>
@@ -38,9 +53,24 @@ function VideoDetail() {
               pt={0}
               textAlign="justify"
               letterSpacing={-0.5}
-              textWrap="no-wrap"
             >
-              {currVid?.snippet?.localized?.description}
+              {currVid?.snippet?.localized?.description.slice(0, len)}
+              <span
+                id="more"
+                onClick={viewDescription}
+                style={{ cursor: "pointer" }}
+                className="text-white cursor-pointer ms-1"
+              >
+                more...
+              </span>
+              <span
+                onClick={hideDescription}
+                id="hide"
+                style={{ display: "none", cursor: "pointer" }}
+                className="text-white cursor-pointer ms-1"
+              >
+                ...hide
+              </span>
             </Typography>
           </Box>
         </Box>

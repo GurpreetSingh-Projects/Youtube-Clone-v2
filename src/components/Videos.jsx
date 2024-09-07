50d7bf83ef35c2037f5e73d9bb5fe64a2eea940c
Gurpreet Singh <gpsingh02414@gmail.com>
2024-09-07 23:00:00 +0530

    bug fixes

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,24 +1,17 @@
 import { Box, Stack } from "@mui/material";
 import React from "react";
 import { ChannelCard, VideoCard } from "./index";
-
 const Videos = ({ videos }, { channelDetail }) => {
   console.log(videos);
   return (
-    <Stack
-      direction="row"
-      flexWrap="wrap"
-      justifyContent="start"
-      gap={2}
-      className="videoCardContainer"
-    >
+    <div className="videos">
       {videos.map((item, idx) => (
-        <Box key={idx}>
+        <Box className="videoCard" key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
-    </Stack>
+    </div>
   );
 };
 

0e370ff0cdbab6bc57dcc2fa1e20d2d9ec58ad37
Gurpreet Singh <gpsingh02414@gmail.com>
2024-09-12 23:00:00 +0530

    bug fixes

--- src/utils/fetchApi.jsx
+++ src/utils/fetchApi.jsx
@@ -3,7 +3,7 @@ const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 const options = {
   method: "GET",
   params: {
-    maxResults: "1",
+    maxResults: "30",
   },
   headers: {
     "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,

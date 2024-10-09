1fbb5f2b586f6ead22ed21f618c3271eff215509
Gurpreet Singh <gpsingh02414@gmail.com>
2024-10-09 23:00:00 +0530

    code update

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Test } from "./components/Test.jsx";
+import Footer from "./components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <App />
+
     {/* <Test /> */}
   </>
 );

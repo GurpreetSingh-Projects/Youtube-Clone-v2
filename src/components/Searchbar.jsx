b663399f8476a07ba1f672afcd1d130607fcc749
Gurpreet Singh <gpsingh02414@gmail.com>
2024-10-10 23:00:00 +0530

    code optimizations and bug fixesp

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,42 +1,56 @@
-import { Paper, IconButton } from "@mui/material";
+import { Paper, IconButton, Stack, Grow } from "@mui/material";
 import { Search } from "@mui/icons-material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 export default function Searchbar() {
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (searchTerm) {
       navigate(`/search/${searchTerm}`);
     }
   };
   return (
-    <Paper
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{
-        borderRadius: 20,
-        border: "1px solid #e3e3e3",
-        pl: 2,
-        boxShadow: "none",
-        display: "flex",
-        mr: { sm: 5 },
-        width: { md: "30%", sm: "50%" },
-      }}
-    >
-      <input
-        className="search-bar"
-        style={{ width: "85%", background: "transparent" }}
-        placeholder="Search..."
-        onChange={(e) => {
-          setSearchTerm(e.target.value);
-        }}
-      />
-      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
-        <Search />
-      </IconButton>
-    </Paper>
+    <Grow in={1}>
+      <Stack
+        direction="row"
+        className="col-4 d-flex ms-3 justify-content-center"
+      >
+        <Paper
+          id="searchBarContainer"
+          className="w-100 animate__flash py-0"
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            borderRadius: 20,
+            border: "1px solid #e3e3e3",
+            pl: 2,
+            boxShadow: "none",
+            display: "flex",
+            mr: { sm: 5 },
+            width: { md: "30%", sm: "50%" },
+            transform: "scale(0.9)",
+          }}
+        >
+          <input
+            className=" search-bar"
+            style={{ background: "transparent" }}
+            placeholder="Search..."
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+          <IconButton
+            className="d-flex"
+            type="submit"
+            sx={{ p: "10px", color: "red" }}
+          >
+            <Search />
+          </IconButton>
+        </Paper>
+      </Stack>
+    </Grow>
   );
 }

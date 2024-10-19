fb18c13fd2c8ce72af016ed3945e1be38373e3d2
Gurpreet Singh <gpsingh02414@gmail.com>
2024-10-19 23:00:00 +0530

    code optimizations and bug fixes

--- src/components/VidSkeleton.jsx
+++ src/components/VidSkeleton.jsx
@@ -1,23 +1,113 @@
 import { Skeleton } from "@mui/material";
-import React from "react";
-
 const VidSkeleton = () => {
   return (
-    <div className="skeleton bg-red">
-      <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
-      <div className="d-flex mt-3 ">
-        <Skeleton
-          className="me-3"
-          variant="circular"
-          sx={{ width: 50, height: 50 }}
-        />
-        <div className="">
+    <div className="skeleton">
+      <div className="skeletonWrapper">
+        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+        <div className="d-flex mt-3 ">
           <Skeleton
-            className="mb-2"
-            variant="rectangular"
-            sx={{ height: 25 }}
+            className="me-3"
+            variant="circular"
+            sx={{ width: 50, height: 50 }}
           />
-          <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+          <div className="">
+            <Skeleton
+              className="mb-2"
+              variant="rectangular"
+              sx={{ height: 25 }}
+            />
+            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+          </div>
+        </div>
+      </div>
+      <div className="skeletonWrapper">
+        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+        <div className="d-flex mt-3 ">
+          <Skeleton
+            className="me-3"
+            variant="circular"
+            sx={{ width: 50, height: 50 }}
+          />
+          <div className="">
+            <Skeleton
+              className="mb-2"
+              variant="rectangular"
+              sx={{ height: 25 }}
+            />
+            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+          </div>
+        </div>
+      </div>
+      <div className="skeletonWrapper">
+        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+        <div className="d-flex mt-3 ">
+          <Skeleton
+            className="me-3"
+            variant="circular"
+            sx={{ width: 50, height: 50 }}
+          />
+          <div className="">
+            <Skeleton
+              className="mb-2"
+              variant="rectangular"
+              sx={{ height: 25 }}
+            />
+            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+          </div>
+        </div>
+      </div>
+      <div className="skeletonWrapper">
+        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+        <div className="d-flex mt-3 ">
+          <Skeleton
+            className="me-3"
+            variant="circular"
+            sx={{ width: 50, height: 50 }}
+          />
+          <div className="">
+            <Skeleton
+              className="mb-2"
+              variant="rectangular"
+              sx={{ height: 25 }}
+            />
+            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+          </div>
+        </div>
+      </div>
+      <div className="skeletonWrapper">
+        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+        <div className="d-flex mt-3 ">
+          <Skeleton
+            className="me-3"
+            variant="circular"
+            sx={{ width: 50, height: 50 }}
+          />
+          <div className="">
+            <Skeleton
+              className="mb-2"
+              variant="rectangular"
+              sx={{ height: 25 }}
+            />
+            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+          </div>
+        </div>
+      </div>
+      <div className="skeletonWrapper">
+        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+        <div className="d-flex mt-3 ">
+          <Skeleton
+            className="me-3"
+            variant="circular"
+            sx={{ width: 50, height: 50 }}
+          />
+          <div className="">
+            <Skeleton
+              className="mb-2"
+              variant="rectangular"
+              sx={{ height: 25 }}
+            />
+            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+          </div>
         </div>
       </div>
     </div>

4e282350125cfd8564b8c2ce95f5d7c8263de102
Gurpreet Singh <gpsingh02414@gmail.com>
2024-10-20 23:00:00 +0530

    moved to youtube official api

--- src/utils/fetchApi.jsx
+++ src/utils/fetchApi.jsx
@@ -1,17 +1,18 @@
 import axios from "axios";
-const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const BASE_URL = "https://www.googleapis.com/youtube/v3";
+
 const options = {
   method: "GET",
   params: {
-    maxResults: "1",
-  },
-  headers: {
-    "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
-    "X-RapidAPI-Host": import.meta.env.VITE_RAPID_API_HOST,
+    maxResults: "20",
   },
 };
 
 export const fetchApi = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  const { data } = await axios.get(
+    `${BASE_URL}/${url}&key=${import.meta.env.VITE_API_KEY}`,
+    options
+  );
   return data;
 };

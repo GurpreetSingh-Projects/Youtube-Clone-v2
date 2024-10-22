58e1928f831ca894287445262eb6fa739649fc6b
Gurpreet Singh <gpsingh02414@gmail.com>
2024-10-22 23:00:00 +0530

    Improve Logo #12

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,22 @@
-import logo from "/assets/images/logo.png";
+import logo from "/assets/images/logo.jpg";
 import { Link } from "react-router-dom";
 import { Searchbar } from "./index";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CreateContext } from "../App";
-import { Paper, IconButton, Stack, Zoom, Grow } from "@mui/material";
+import { Paper, IconButton, Stack, Zoom, Grow, Box } from "@mui/material";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Search } from "@mui/icons-material";
+import { Menu, Search } from "@mui/icons-material";
 const Navbar = () => {
-  const { setSelectedCategory } = useContext(CreateContext);
+  const { setSelectedCategory, sidebar, setSidebar } =
+    useContext(CreateContext);
   function resetState() {
     setSelectedCategory("New");
   }
-
+  function toggleSidebar() {
+    setSidebar(!sidebar);
+  }
   return (
     <Stack
       className="px-3 py-2"
@@ -27,38 +30,42 @@ const Navbar = () => {
         background: "inherit",
       }}
     >
-      <Link
-        to="/"
-        className="col-4"
-        onClick={resetState}
-        sx={{ display: "flex", alignItems: "center" }}
-      >
-        <Grow in={true} style={{ transitionDelay: "1500ms" }}>
-          <img className="logoImg" src={logo} alt="logo" />
-        </Grow>
-      </Link>
-      <Searchbar />
+      <Box className="col-4">
+        <Link
+          to="/"
+          className="d-flex align-items-center justify-between"
+          onClick={resetState}
+          sx={{ display: "flex", alignItems: "center" }}
+        >
+          <Grow in={true} style={{ transitionDelay: "1500ms" }}>
+            <img className="logoImg animate__bounce" src={logo} alt="logo" />
+          </Grow>
+        </Link>
+      </Box>
+      <Box className="col-4">
+        <Searchbar />
+      </Box>
+      <Box className="col-4">
+        <Stack
+          className="col-0 d-flex align-items-center justify-content-end"
+          direction="row"
+        >
+          <Grow in={true} style={{ transitionDelay: "1500ms" }}>
+            <Box className="d-flex align-items-center gap-3">
+              <IconButton className="text-white">
+                <HelpOutlineIcon />
+              </IconButton>
+              <IconButton className="text-white">
+                <NotificationsIcon />
+              </IconButton>
 
-      <Stack
-        className="gap-3 col-4 d-flex align-items-center justify-content-end"
-        direction="row"
-      >
-        <Grow in={true} style={{ transitionDelay: "1500ms" }}>
-          <IconButton className="text-white">
-            <HelpOutlineIcon />
-          </IconButton>
-        </Grow>
-        <Grow in={true} style={{ transitionDelay: "1500ms" }}>
-          <IconButton className="text-white">
-            <NotificationsIcon />
-          </IconButton>
-        </Grow>
-        <Grow in={true} style={{ transitionDelay: "1500ms" }}>
-          <IconButton className="text-white">
-            <AccountCircleIcon />
-          </IconButton>
-        </Grow>
-      </Stack>
+              <IconButton className="text-white">
+                <AccountCircleIcon />
+              </IconButton>
+            </Box>
+          </Grow>
+        </Stack>
+      </Box>
     </Stack>
   );
 };

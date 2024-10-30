e6bdf4371544abaad295edc1c3d11e2ffbc93b58
Gurpreet Singh <gpsingh02414@gmail.com>
2024-10-30 23:00:00 +0530

    code optimizations and bug fixes

--- src/components/VidSkeleton.jsx
+++ src/components/VidSkeleton.jsx
@@ -2,111 +2,159 @@ import { Skeleton } from "@mui/material";
 const VidSkeleton = () => {
   return (
     <div className="d-flex flex-wrap">
-      <div className="skeletonWrapper col-12 col-md-4 bg-gray">
-        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+      <div className="skeletonWrapper col-12 col-md-4 p-3">
+        <Skeleton
+          className="skeletonElement bg-body-secondary opacity-50"
+          variant="rectangular"
+          sx={{ height: 180, margin: 1 }}
+        />
         <div className="d-flex mt-3 ">
           <Skeleton
-            className="me-3"
+            className="skeletonElement bg-body-secondary opacity-5 me-3"
             variant="circular"
             sx={{ width: 50, height: 50 }}
           />
           <div className="">
             <Skeleton
-              className="mb-2"
+              className="skeletonElement bg-body-secondary opacity-5 mb-2"
               variant="rectangular"
               sx={{ height: 25 }}
             />
-            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+            <Skeleton
+              className="skeletonElement bg-body-secondary opacity-5"
+              variant="rectangular"
+              sx={{ width: 270, height: 15 }}
+            />
           </div>
         </div>
       </div>
-      <div className="skeletonWrapper col-12 col-md-4 bg-gray">
-        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+      <div className="skeletonWrapper col-12 col-md-4 p-3">
+        <Skeleton
+          className="skeletonElement bg-body-secondary opacity-5"
+          variant="rectangular"
+          sx={{ height: 180, margin: 1 }}
+        />
         <div className="d-flex mt-3 ">
           <Skeleton
-            className="me-3"
+            className="skeletonElement bg-body-secondary opacity-5 me-3"
             variant="circular"
             sx={{ width: 50, height: 50 }}
           />
           <div className="">
             <Skeleton
-              className="mb-2"
+              className="skeletonElement bg-body-secondary opacity-5 mb-2"
               variant="rectangular"
               sx={{ height: 25 }}
             />
-            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+            <Skeleton
+              className="skeletonElement bg-body-secondary opacity-5"
+              variant="rectangular"
+              sx={{ width: 270, height: 15 }}
+            />
           </div>
         </div>
       </div>
-      <div className="skeletonWrapper col-12 col-md-4 bg-gray">
-        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+      <div className="skeletonWrapper col-12 col-md-4 p-3">
+        <Skeleton
+          className="skeletonElement bg-body-secondary opacity-5"
+          variant="rectangular"
+          sx={{ height: 180, margin: 1 }}
+        />
         <div className="d-flex mt-3 ">
           <Skeleton
-            className="me-3"
+            className="skeletonElement bg-body-secondary opacity-5 me-3"
             variant="circular"
             sx={{ width: 50, height: 50 }}
           />
           <div className="">
             <Skeleton
-              className="mb-2"
+              className="skeletonElement bg-body-secondary opacity-5 mb-2"
               variant="rectangular"
               sx={{ height: 25 }}
             />
-            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+            <Skeleton
+              className="skeletonElement bg-body-secondary opacity-5"
+              variant="rectangular"
+              sx={{ width: 270, height: 15 }}
+            />
           </div>
         </div>
       </div>
-      <div className="skeletonWrapper col-12 col-md-4 bg-gray">
-        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+      <div className="skeletonWrapper col-12 col-md-4 p-3">
+        <Skeleton
+          className="skeletonElement bg-body-secondary opacity-5"
+          variant="rectangular"
+          sx={{ height: 180, margin: 1 }}
+        />
         <div className="d-flex mt-3 ">
           <Skeleton
-            className="me-3"
+            className="skeletonElement bg-body-secondary opacity-5 me-3"
             variant="circular"
             sx={{ width: 50, height: 50 }}
           />
           <div className="">
             <Skeleton
-              className="mb-2"
+              className="skeletonElement bg-body-secondary opacity-5 mb-2"
               variant="rectangular"
               sx={{ height: 25 }}
             />
-            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+            <Skeleton
+              className="skeletonElement bg-body-secondary opacity-5"
+              variant="rectangular"
+              sx={{ width: 270, height: 15 }}
+            />
           </div>
         </div>
       </div>
-      <div className="skeletonWrapper col-12 col-md-4 bg-gray">
-        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+      <div className="skeletonWrapper col-12 col-md-4 p-3">
+        <Skeleton
+          className="skeletonElement bg-body-secondary opacity-5"
+          variant="rectangular"
+          sx={{ height: 180, margin: 1 }}
+        />
         <div className="d-flex mt-3 ">
           <Skeleton
-            className="me-3"
+            className="skeletonElement bg-body-secondary opacity-5 me-3"
             variant="circular"
             sx={{ width: 50, height: 50 }}
           />
           <div className="">
             <Skeleton
-              className="mb-2"
+              className="skeletonElement bg-body-secondary opacity-5 mb-2"
               variant="rectangular"
               sx={{ height: 25 }}
             />
-            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+            <Skeleton
+              className="skeletonElement bg-body-secondary opacity-5"
+              variant="rectangular"
+              sx={{ width: 270, height: 15 }}
+            />
           </div>
         </div>
       </div>
-      <div className="skeletonWrapper col-12 col-md-4 bg-gray">
-        <Skeleton variant="rectangular" sx={{ height: 180, margin: 1 }} />
+      <div className="skeletonWrapper col-12 col-md-4 p-3">
+        <Skeleton
+          className="skeletonElement bg-body-secondary opacity-5"
+          variant="rectangular"
+          sx={{ height: 180, margin: 1 }}
+        />
         <div className="d-flex mt-3 ">
           <Skeleton
-            className="me-3"
+            className="skeletonElement bg-body-secondary opacity-5 me-3"
             variant="circular"
             sx={{ width: 50, height: 50 }}
           />
           <div className="">
             <Skeleton
-              className="mb-2"
+              className="skeletonElement bg-body-secondary opacity-5 mb-2"
               variant="rectangular"
               sx={{ height: 25 }}
             />
-            <Skeleton variant="rectangular" sx={{ width: 270, height: 15 }} />
+            <Skeleton
+              className="skeletonElement bg-body-secondary opacity-5"
+              variant="rectangular"
+              sx={{ width: 270, height: 15 }}
+            />
           </div>
         </div>
       </div>

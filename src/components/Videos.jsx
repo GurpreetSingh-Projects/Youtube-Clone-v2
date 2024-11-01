2e85ff24aec33c3db715c9d480d4c6b0b3c8a4af
Gurpreet Singh <gpsingh02414@gmail.com>
2024-11-01 23:00:00 +0530

    Implement new ui standard for showing video details on dashboard #4

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,27 +2,49 @@ import { Box, Stack } from "@mui/material";
 import React, { createContext, useContext } from "react";
 import { ChannelCard, ChannelDetail, VideoCard } from "./index";
 import { CreateContext } from "../App";
+import { motion } from "framer-motion";
 const Videos = ({ suggested }) => {
   const { videos } = useContext(CreateContext);
   // console.log(videos);
 
+  const slideUp = {
+    hidden: { opacity: 1, y: -100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        staggerChildren: 0.05,
+        duration: 0.3,
+      },
+    },
+  };
   return suggested ? (
-    <div className="suggestedvideos">
+    <motion.div
+      inital="hidden"
+      animate="visible"
+      variants={slideUp}
+      className="suggestedvideos"
+    >
       {videos.map((item, idx) => (
         <Box className="videoCard" key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
         </Box>
       ))}
-    </div>
+    </motion.div>
   ) : (
-    <div className="videos">
+    <motion.div
+      inital="hidden"
+      animate="visible"
+      variant="slideUp"
+      className="videos"
+    >
       {videos.map((item, idx) => (
         <Box className="videoCard" key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
-    </div>
+    </motion.div>
   );
 };
 

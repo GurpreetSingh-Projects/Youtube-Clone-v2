ae38100ee2717d8ed5006c604bcce695401245b7
Gurpreet Singh <gpsingh02414@gmail.com>
2024-11-05 23:00:00 +0530

    Implement Redux for better state management #14

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Test } from "./components/Test.jsx";
 import Footer from "./components/Footer.jsx";
-
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <App />
-
+    <Provider store={store}>
+      <App />
+    </Provider>
     {/* <Test /> */}
   </>
 );

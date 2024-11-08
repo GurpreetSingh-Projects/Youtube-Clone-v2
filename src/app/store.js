541168ac566dfe3f5e0d0b08627fc5abeb99b918
Gurpreet Singh <gpsingh02414@gmail.com>
2024-11-08 23:00:00 +0530

    Implement Redux for better state management #14

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import searchbarReducer from "../features/Searchbar/searchbarSlice";
+import categoryReducer from "../features/Category/categorySlice";
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     searchbar: searchbarReducer,
+    category: categoryReducer,
   },
 });

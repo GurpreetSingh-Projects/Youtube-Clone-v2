39f4fc6277ffe8efbcdd2880d43d0487b908d117
Gurpreet Singh <gpsingh02414@gmail.com>
2024-11-26 21:25:15 +0530

    Add and optimize modals for Profile, Help, and Notification buttons (#53, #54, #55, #56, #57, #58)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,68 @@
 import logo from "/assets/images/logo.jpg";
-import { Link } from "react-router-dom";
-import { Searchbar } from "./index";
-import { useContext } from "react";
-import { CreateContext } from "../App";
-import { Paper, IconButton, Stack, Box, Grow } from "@mui/material";
-import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { Link, Navigate } from "react-router-dom";
+import {
+  Searchbar,
+  NewReleasesIcon,
+  HelpOutlineIcon,
+  NotificationsIcon,
+  AccountCircleIcon,
+  MenuIcon,
+  CloseIcon,
+  ExpandMoreIcon,
+} from "./index";
+import {
+  IconButton,
+  Stack,
+  Box,
+  Grow,
+  Typography,
+  Modal,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Avatar,
+} from "@mui/material";
 import { Search } from "@mui/icons-material";
-import MenuIcon from "@mui/icons-material/Menu";
 import { AnimatePresence, motion } from "framer-motion";
 import { Typewriter } from "react-simple-typewriter";
 import { useDispatch, useSelector } from "react-redux";
 import { searchbarToggle } from "../features/Searchbar/searchbarSlice";
 import { setCategory } from "../features/Category/categorySlice";
+import { useState } from "react";
 
 const Navbar = () => {
   const dispatch = useDispatch();
+  const [modalManager, setModalManager] = useState({
+    helpModal: false,
+    notifyModal: true,
+    profileModal: false,
+  });
+  // const { sidebar, setSidebar } = useContext(CreateContext);
 
-  const { sidebar, setSidebar } = useContext(CreateContext);
+  function showModal(modalName) {
+    setModalManager((prev) => ({
+      [modalName]: !prev[modalName],
+    }));
+  }
 
   function resetState() {
     dispatch(setCategory(state.category.initialState));
   }
 
-  function toggleSidebar() {
-    setSidebar(!sidebar);
-  }
-
+  // function toggleSidebar() {
+  //   setSidebar(!sidebar);
+  // }
+  const style = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 500,
+    color: "white",
+    bgcolor: "#1E1E1E",
+    p: 4,
+    borderRadius: "20px",
+  };
   const containerVariants = {
     hidden: {
       scale: 0.5,
@@ -49,6 +84,9 @@ const Navbar = () => {
   function toggleSearch() {
     dispatch(searchbarToggle());
   }
+  const replaceUrl = (url) => {
+    Navigate(url, { replace: true });
+  };
   const searchbar = useSelector((state) => state.searchbar);
   return (
     <Stack
@@ -135,22 +173,333 @@ const Navbar = () => {
             </IconButton>
           </Grow>
           <Grow in={true}>
-            <IconButton className="text-white">
+            <IconButton
+              className="text-white"
+              onClick={() => {
+                showModal("helpModal");
+              }}
+            >
               <HelpOutlineIcon />
             </IconButton>
           </Grow>
+
           <Grow in={true}>
-            <IconButton className="text-white">
+            <IconButton
+              className="text-white"
+              onClick={() => {
+                showModal("notifyModal");
+              }}
+            >
               <NotificationsIcon />
             </IconButton>
           </Grow>
           <Grow in={true}>
-            <IconButton className="text-white">
+            <IconButton
+              className="text-white"
+              onClick={() => {
+                showModal("profileModal");
+              }}
+            >
               <AccountCircleIcon />
             </IconButton>
           </Grow>
         </Stack>
       </Box>
+      <Modal
+        open={modalManager.helpModal}
+        onClose={() => {
+          showModal("helpModal");
+        }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          transition={{ duration: 0.3 }}
+          variants={containerVariants}
+          className="position-fixed"
+          style={{ inset: 0 }}
+        >
+          <Box sx={style} className="modalWrapper">
+            <Box
+              className="closeButton"
+              onClick={() => {
+                showModal("helpModal");
+              }}
+            >
+              <IconButton>
+                <CloseIcon />
+              </IconButton>
+            </Box>
+            <Typography
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
+              sx={{ mb: "15px" }}
+            >
+              Help&nbsp;
+              <HelpOutlineIcon />
+            </Typography>
+            <Accordion className="accordionHeader">
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                <Typography>
+                  I can&apos;t find the link to Github Repository
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails className="accordionPara">
+                <Typography>
+                  This Project is Closed Source due to its vast nature, and
+                  complex api usage. This project not only incorporates UI built
+                  through React, but also widely uses other libraries like
+                  Material UI, Bootstrap, Framer Motion, Redux for state
+                  management etc. as well
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion className="accordionHeader">
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+              >
+                <Typography>What is this YouTube clone </Typography>
+              </AccordionSummary>
+              <AccordionDetails className="accordionPara">
+                <Typography>
+                  This YouTube clone is a web application built with React that
+                  mimics the core functionalities of YouTube. It includes
+                  features like video playback, search functionality, dynamic
+                  content updates, comments, and more, all styled using Material
+                  UI (MUI) and Bootstrap. The app is hosted on Vercel, offering
+                  fast and reliable performance.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion className="accordionHeader">
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+              >
+                <Typography>
+                  What technologies are used in building this YouTube clone?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails className="accordionPara">
+                <Typography>
+                  This app uses the following technologies:
+                  <br />
+                  <ol>
+                    <li>React Router DOM for routing and navigation.</li>
+                    <li>
+                      Material UI (MUI) and Bootstrap for the user interface
+                      components and styling.
+                    </li>
+                    <li>YouTube API for fetching videos and related data. </li>
+                    <li>Vercel for hosting the app.</li>
+                  </ol>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion className="accordionHeader">
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+              >
+                <Typography>
+                  Does the app have a mobile-friendly design?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails className="accordionPara">
+                <Typography>
+                  Yes, the app is fully responsive, utilizing Material UI and
+                  Bootstrap to ensure that it works well on all screen sizes,
+                  including mobile devices and tablets. The user interface
+                  adapts dynamically to provide a smooth experience on any
+                  device.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion className="accordionHeader">
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+              >
+                <Typography>How do I report bugs or issues?</Typography>
+              </AccordionSummary>
+              <AccordionDetails className="accordionPara">
+                <Typography>
+                  To report bugs or issues, please reach out me at
+                  gpsingh02414@gmail.com
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded className="accordionHeader">
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+              >
+                <Typography>
+                  <b>Hire Me</b>
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails className="accordionPara">
+                <Typography>
+                  You can reach out to me via multiple platforms -
+                  <br />
+                  <Box>
+                    Email:&nbsp;
+                    <a
+                      href="mailto:gpsingh02414@gmail.com"
+                      className="text-white"
+                      target="_blank"
+                    >
+                      gpsingh02414@gmail.com
+                    </a>
+                  </Box>
+                  <Box>
+                    LinkedIn:{" "}
+                    <a
+                      href="https://www.linkedin.com/in/gurpreetchadhaeee/"
+                      className="text-white"
+                      target="_blank"
+                    >
+                      https://www.linkedin.com/in/gurpreetchadhaeee/
+                    </a>
+                  </Box>
+                  <Box>
+                    {" "}
+                    Github:{" "}
+                    <a
+                      href="https://github.com/GurpreetSingh-Projects"
+                      className="text-white"
+                      target="_blank"
+                    >
+                      https://github.com/GurpreetSingh-Projects
+                    </a>
+                  </Box>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </Box>
+        </motion.div>
+      </Modal>
+      <Modal
+        open={modalManager.notifyModal}
+        onClose={() => {
+          showModal("notifyModal");
+        }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          transition={{ duration: 0.3 }}
+          variants={containerVariants}
+          className="position-fixed"
+          style={{ inset: 0 }}
+        >
+          <Box sx={style} className="modalWrapper">
+            <Box
+              className="closeButton"
+              onClick={() => {
+                showModal("notifyModal");
+              }}
+            >
+              <IconButton>
+                <CloseIcon />
+              </IconButton>
+            </Box>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Recent Notifications&nbsp; <NotificationsIcon />
+            </Typography>
+            <Typography
+              id="modal-modal-description"
+              className="notificationContainer"
+              sx={{ mt: 2 }}
+            >
+              <Link to={`/video/Xj0Jtjg3lHQ`}>
+                <Box
+                  className="notificationItem"
+                  onClick={() => {
+                    showModal("notifyModal");
+                  }}
+                >
+                  <Avatar src="https://yt3.googleusercontent.com/fxGKYucJAVme-Yz4fsdCroCFCrANWqw0ql4GYuvx8Uq4l_euNJHgE-w9MTkLQA805vWCi-kE0g=s160-c-k-c0x00ffffff-no-rj"></Avatar>
+                  <Box>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Labore, doloremque!
+                  </Box>
+                  <Box>
+                    <img
+                      src="https://i.ytimg.com/vi/Xj0Jtjg3lHQ/mqdefault.jpg"
+                      alt=""
+                    />
+                  </Box>
+                  <Box
+                    className="text-white position-absolute"
+                    sx={{ top: "-10px", right: "-15px" }}
+                  >
+                    <NewReleasesIcon />
+                  </Box>
+                </Box>
+              </Link>
+            </Typography>
+          </Box>
+        </motion.div>
+      </Modal>
+      <Modal
+        open={modalManager.profileModal}
+        onClose={() => {
+          showModal("profileModal");
+        }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className="modalWrapper overflow-scroll"
+      >
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          transition={{ duration: 0.3 }}
+          variants={containerVariants}
+          className="position-fixed"
+          onClick={() => {
+            showModal("profileModal");
+          }}
+          style={{ inset: 0 }}
+        >
+          <Box sx={style} className="modalWrapper">
+            <Box
+              className="closeButton"
+              onClick={() => {
+                showModal("profileModal");
+              }}
+            >
+              <IconButton>
+                <CloseIcon />
+              </IconButton>
+            </Box>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Profile Modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </motion.div>
+      </Modal>
     </Stack>
   );
 };

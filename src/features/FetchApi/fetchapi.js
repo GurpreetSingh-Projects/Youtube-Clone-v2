29611008962fe9a32dd2aecfc77b9b57180fc473
Gurpreet Singh <gpsingh02414@gmail.com>
2024-12-22 23:00:00 +0530

    Implement Redux for better state management #14

---
+++ src/features/FetchApi/fetchapi.js
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const API_KEY = import.meta.env.VITE_API_KEY;
+const results = 6;
+
+export const youtubeApi = createApi({
+  reducerPath: "youtubeApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://www.googleapis.com/youtube/v3/",
+  }),
+  endpoints: (builder) => ({
+    getVideos: builder.query({
+      query: (searchTerm) => ({
+        url: `search`,
+        method: "GET",
+        params: {
+          part: "snippet",
+          q: searchTerm,
+          key: API_KEY,
+          maxResults: results,
+          type: "video",
+        },
+      }),
+    }),
+    getVidDetails: builder.query({
+      query: (vidIds) => ({
+        url: `videos?part=snippet,contentDetails,statistics&id=${vidIds}&key=${API_KEY}`,
+        method: "GET",
+      }),
+    }),
+    getChannelDetails: builder.query({
+      query: (channelIds) => ({
+        url: `channels?part=snippet&id=${channelIds}&key=${API_KEY}`,
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetVideosQuery,
+  useGetVidDetailsQuery,
+  useGetChannelDetailsQuery,
+} = youtubeApi;

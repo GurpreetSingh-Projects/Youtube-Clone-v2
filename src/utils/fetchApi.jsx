b0f041cc453921fa29fd0eff6aebbe5b4cbeae6a
Gurpreet Singh <gpsingh02414@gmail.com>
2024-12-25 23:00:00 +0530

    Implement Redux for better state management #14

--- src/utils/fetchApi.jsx
+++ src/utils/fetchApi.jsx
@@ -5,13 +5,13 @@ const BASE_URL = "https://www.googleapis.com/youtube/v3";
 const options = {
   method: "GET",
   params: {
-    maxResults: "25",
+    maxResults: "2",
     type: "video",
   },
 };
 
 export const fetchApi = async (url) => {
-  const { data } = await axios.get(
+  var { data } = await axios.get(
     `${BASE_URL}/${url}&key=${import.meta.env.VITE_API_KEY}`,
     options
   );

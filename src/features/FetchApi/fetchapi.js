234c9154c3727352d06e85187762734ee0027569
Gurpreet Singh <gpsingh02414@gmail.com>
2025-01-09 23:00:00 +0530

    Add Comments feature #46

--- src/features/FetchApi/fetchapi.js
+++ src/features/FetchApi/fetchapi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const API_KEY = import.meta.env.VITE_API_KEY;
-const results = 1;
+const results = 10;
 
 export const youtubeApi = createApi({
   reducerPath: "youtubeApi",
@@ -23,13 +23,19 @@ export const youtubeApi = createApi({
     }),
     getVidDetails: builder.query({
       query: (vidIds) => ({
-        url: `videos?part=snippet,contentDetails,statistics&id=${vidIds}&key=${API_KEY}`,
+        url: `videos?part=snippet,contentDetails,statistics&id=${vidIds}&key=${API_KEY}&type=video`,
         method: "GET",
       }),
     }),
     getChannelDetails: builder.query({
       query: (channelIds) => ({
         url: `channels?part=snippet%2Cstatistics%2CbrandingSettings&id=${channelIds}&key=${API_KEY}`,
+        method: "GET",
+      }),
+    }),
+    getComments: builder.query({
+      query: (videoId) => ({
+        url: `commentThreads?part=snippet&videoId=${videoId}&order=relevance&key=${API_KEY}`,
         method: "GET",
       }),
     }),
@@ -40,4 +46,5 @@ export const {
   useGetVideosQuery,
   useGetVidDetailsQuery,
   useGetChannelDetailsQuery,
+  useGetCommentsQuery,
 } = youtubeApi;

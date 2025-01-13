f4f2761fcccf1031c08eff880964a3ce14355dc9
Gurpreet Singh <gpsingh02414@gmail.com>
2025-01-13 23:00:00 +0530

    version bump

--- src/features/FetchApi/fetchapi.js
+++ src/features/FetchApi/fetchapi.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const API_KEY = import.meta.env.VITE_API_KEY;
 const results = 25;
-
 export const youtubeApi = createApi({
   reducerPath: "youtubeApi",
   baseQuery: fetchBaseQuery({

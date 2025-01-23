f3031e9eab7cdd3c059e0afc80f0e5b4762e18cc
Gurpreet Singh <gpsingh02414@gmail.com>
2025-01-23 23:00:00 +0530

    Fix Channel Details Page bugs #50

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,27 +1,55 @@
-import { useContext, useEffect, useState } from "react";
+// import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchApi } from "../utils/fetchApi";
+// import { fetchApi } from "../utils/fetchApi";
 import { Box, Card, Hidden, Typography } from "@mui/material";
 import { ChannelCard, Videos } from "./index";
-import { CreateContext } from "../App";
+// import { CreateContext } from "../App";
+import { useGetVidDetailsQuery } from "../features/FetchApi/fetchapi";
+import { useEffect, useState } from "react";
+import { fetchApi } from "../utils/fetchApi";
+import { useDispatch } from "react-redux";
+import { setVideos } from "../features/Videos/videoSlice";
+import { setChannelIds } from "../features/ChannelIds/channelidsSlice";
+import SubscribeButton from "./SubscribeButton";
+import { extendDescription } from "../utils/constants";
 
 const ChannelDetail = () => {
   const [channelDetail, setChannelDetail] = useState(null);
-  const { setVideos } = useContext(CreateContext);
+  const [channelPlaylistId, setChannelPlaylistId] = useState("");
+  var playListId = "";
+  // const { setVideos } = useContext(CreateContext);
 
   const { id } = useParams();
-
+  const dispatch = useDispatch;
   useEffect(() => {
-    fetchApi(`channels?part=snippet&id=${id}`).then((data) => {
+    fetchApi(
+      `channels?part=snippet%2Cstatistics%2CbrandingSettings&id=${id}`
+    ).then((data) => {
       setChannelDetail(data?.items[0]);
     });
 
-    fetchApi(`search?channelId=${id}&part=snippet%2Cid&order=date`).then(
-      (data) => {
-        setVideos(data.items);
-      }
-    );
+    fetchApi(`channels?part=contentDetails&id=${id}`).then((data) => {
+      setChannelPlaylistId(
+        data?.items[0]?.contentDetails?.relatedPlaylists?.uploads
+      );
+      playListId = data?.items[0]?.contentDetails?.relatedPlaylists?.uploads;
+    });
+
+    // fetchApi(`search?part=snippet&channelId?=${id}&type=video`).then((data) => {
+    //   // alert("videos-" + JSON.stringify(data));
+    //   dispatch(setChannelIds(data));
+    // });
   }, [id]);
+  useEffect(() => {
+    fetchApi(`playlistItems?part=snippet&id=${playListId}`).then((data) => {
+      alert(playListId);
+      console.log(JSON.stringify(data));
+    });
+  }, [playListId]);
+
+  // const { data: getChannelDetails } = useGetVidDetailsQuery(id);
+  // console.log(JSON.stringify(getChannelDetails));
+
   return (
     <>
       <Box>
@@ -50,14 +78,15 @@ const ChannelDetail = () => {
           marginTop="-100px"
         ></ChannelCard>
       </Box>
-      <Box className="d-flex align-items-center justify-content-center">
+      <Box className="faj flex-column">
         <Typography
+          id="vidDescription"
+          onClick={extendDescription}
           style={{
             fontSize: "15px",
             color: "white",
             textAlign: "center",
             margin: "20px",
-
             marginTop: { lg: "-20px", sm: "-50px" },
             maxWidth: "75vw",
             textWrap: "wrap",
@@ -66,7 +95,11 @@ const ChannelDetail = () => {
         >
           {channelDetail?.brandingSettings?.channel?.description}
         </Typography>
+        <Box className="faj">
+          <SubscribeButton marginStart="ms-0" />
+        </Box>
       </Box>
+
       <Box className="d-flex align-items-center justify-content-center p-3">
         <Videos />
       </Box>

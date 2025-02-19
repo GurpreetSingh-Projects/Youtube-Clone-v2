bc500543aa64b98b740ffbc946e2a4bb22c2bfaa
Gurpreet Singh <gpsingh02414@gmail.com>
2025-02-19 23:00:00 +0530

    Refactor Code for More Speed, Code reusage #59

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,16 +1,41 @@
-import React from "react";
 import ReactPlayer from "react-player";
-
+import { motion } from "framer-motion";
 const VideoPlayer = (props) => {
+  const slideUp = {
+    hidden: { opacity: 0.5 },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.15,
+      },
+    },
+  };
   return (
-    <ReactPlayer
-      className={`react-player mb-3 mb-md-0 ${props?.videoCardCall}`}
-      url={`https://www.youtube.com/watch?v=${props?.videoId}`}
-      playing={true}
-      muted
-      controls={!props?.videoCardCall}
-      style={{ zIndex: -1 }}
-    />
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={slideUp}
+      exit="exit"
+    >
+      <ReactPlayer
+        className={`react-player mb-3 mb-md-0 ${props?.videoCardCall}`}
+        url={`https://www.youtube.com/watch?v=${props?.videoId}`}
+        playing={true}
+        muted
+        controls={!props?.videoCardCall}
+        style={{ zIndex: -1 }}
+        config={{
+          youtube: {
+            playerVars: {
+              cc_load_policy: 1,
+              cc: "en",
+              rel: 0,
+            },
+          },
+        }}
+      />
+    </motion.div>
   );
 };
 

3012d075203773be90a52f4b6a158bf6781ad3f9
Gurpreet Singh <gpsingh02414@gmail.com>
2025-03-26 23:00:00 +0530

    Improve Timed Out UI #70

--- src/components/TimedOut.jsx
+++ src/components/TimedOut.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
+import ErrorIcon from "@mui/icons-material/Error";
+import { Box, Icon, Typography } from "@mui/material";
 const TimedOut = () => {
-  return <div className="faj text-white">Oops Timeout</div>;
+  return (
+    <div className="faj text-white h-100 w-100">
+      <Box className="faj flex-column errorBox">
+        <ErrorIcon sx={{ fontSize: 100 }} />
+        <Typography sx={{ marginTop: 1 }} variant="subtitle">
+          OOPS!! we ran into an error
+        </Typography>
+      </Box>
+    </div>
+  );
 };
 
 export default TimedOut;

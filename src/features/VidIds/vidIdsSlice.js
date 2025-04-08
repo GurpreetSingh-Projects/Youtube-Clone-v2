48a8db3acc4de05aae010aee8a296b52308d5100
Gurpreet Singh <gpsingh02414@gmail.com>
2025-04-08 23:00:00 +0530

    Fix Channel Details Page bugs #50 Open

--- src/features/VidIds/vidIdsSlice.js
+++ src/features/VidIds/vidIdsSlice.js
@@ -4,12 +4,16 @@ export const vidIdsSlice = createSlice({
   name: "vidIds",
   initialState: {
     vidIds: [],
+    currVidId: "",
   },
   reducers: {
     setVidIds: (state, action) => {
       state.vidIds = action.payload;
     },
+    setCurrVidId: (state, action) => {
+      state.currVidId = action.payload;
+    },
   },
 });
-export const { setVidIds } = vidIdsSlice.actions;
+export const { setVidIds, setCurrVidId } = vidIdsSlice.actions;
 export default vidIdsSlice.reducer;

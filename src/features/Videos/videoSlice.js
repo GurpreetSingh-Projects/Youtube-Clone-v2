85c923f7f3f8029663995f43e1506df53471efb3
Gurpreet Singh <gpsingh02414@gmail.com>
2025-04-08 23:00:00 +0530

    Fix Channel Details Page bugs #50 Open

--- src/features/Videos/videoSlice.js
+++ src/features/Videos/videoSlice.js
@@ -3,13 +3,17 @@ export const videoSlice = createSlice({
   name: "videos",
   initialState: {
     videos: [],
+    recommendedVideos: [],
   },
   reducers: {
     setVideos: (state, action) => {
       state.videos = action.payload;
     },
+    setRecommendedVideos: (state, action) => {
+      state.recommendedVideos = action.payload;
+    },
   },
 });
 
-export const { setVideos } = videoSlice.actions;
+export const { setVideos, setRecommendedVideos } = videoSlice.actions;
 export default videoSlice.reducer;

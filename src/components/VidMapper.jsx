4d89d48042c7492babd8c9c6eb71cab03e840eb8
Gurpreet Singh <gpsingh02414@gmail.com>
2025-04-13 23:00:00 +0530

    Refactor Code for More Speed, Code reusage #59

---
+++ src/components/VidMapper.jsx
@@ -0,0 +1,62 @@
+import { motion } from "framer-motion";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
+const VidMapper = (props) => {
+  const innerVariant = {
+    hidden: {
+      opacity: 0.8,
+      scale: 0.9,
+      y: 0,
+    },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+  const outerVariant = {
+    hidden: {
+      scale: 0.95,
+      y: 10,
+      opacity: 0.5,
+    },
+    visible: {
+      scale: 1,
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.05,
+      },
+    },
+    exit: {
+      opacity: 0.5,
+      scale: 0,
+    },
+  };
+  return (
+    <>
+      <motion.div
+        variants={outerVariant}
+        className={`${props?.class}`}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        {props?.videos?.items.map((item, idx) => (
+          <motion.div variants={innerVariant} key={idx}>
+            {item.id && (
+              <VideoCard video={item} channelView={props?.channelView} />
+            )}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </motion.div>
+        ))}
+      </motion.div>
+    </>
+  );
+};
+
+export default VidMapper;

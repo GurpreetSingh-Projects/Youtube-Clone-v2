fde3baef91c34b5da5a2612d32dd1578c15ff1fa
Gurpreet Singh <gpsingh02414@gmail.com>
2025-04-21 23:00:00 +0530

    Improve No Comments found UI #72

--- src/components/TimedOut.jsx
+++ src/components/TimedOut.jsx
@@ -1,12 +1,12 @@
 import ErrorIcon from "@mui/icons-material/Error";
 import { Box, Icon, Typography } from "@mui/material";
-const TimedOut = () => {
+const TimedOut = (props) => {
   return (
     <div className="faj text-white h-100 w-100">
       <Box className="faj flex-column errorBox">
         <ErrorIcon sx={{ fontSize: 100 }} />
         <Typography sx={{ marginTop: 1 }} variant="subtitle">
-          OOPS!! we ran into an error
+          {props.comments ? "No Comments Found" : "OOPS!! we ran into an error"}
         </Typography>
       </Box>
     </div>

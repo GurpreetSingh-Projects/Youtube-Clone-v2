4d77ab26281ede0d6709c28b3bf536991bb7c002
Gurpreet Singh <gpsingh02414@gmail.com>
2025-04-27 23:00:00 +0530

    Improve UI and Mobile Responsiveness #71

--- src/components/TimedOut.jsx
+++ src/components/TimedOut.jsx
@@ -5,7 +5,11 @@ const TimedOut = (props) => {
     <div className="faj text-white h-100 w-100">
       <Box className="faj flex-column errorBox">
         <ErrorIcon sx={{ fontSize: 100 }} />
-        <Typography sx={{ marginTop: 1 }} variant="subtitle">
+        <Typography
+          sx={{ marginTop: 1 }}
+          variant="subtitle"
+          className="text-center"
+        >
           {props.comments ? "No Comments Found" : "OOPS!! we ran into an error"}
         </Typography>
       </Box>

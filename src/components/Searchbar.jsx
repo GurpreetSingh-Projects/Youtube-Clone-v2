fa139002a97510c7486b1b1cbef5b95fa0a4026a
Gurpreet Singh <gpsingh02414@gmail.com>
2025-05-09 23:00:00 +0530

    Refactor Code for More Speed, Code reusage #59

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,16 +1,15 @@
-import { Paper, IconButton, Box, Grow, Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
 import { Close, Search } from "@mui/icons-material";
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { searchbarToggle } from "../features/Searchbar/searchbarSlice";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
-import { animate, motion, stagger } from "framer-motion";
+import { motion } from "framer-motion";
+// import { geminiApi2 } from "../features/FetchApi/geminiApi";
 export default function Searchbar() {
   const [searchTerm, setSearchTerm] = useState("");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  var searchTerms = [
+  var searchRecommendation = [
     "AI music composition",
     "virtual concerts",
     "Spotify music algorithm",
@@ -27,17 +26,11 @@ export default function Searchbar() {
     "Photos App",
     "Portfolio",
   ];
-  useMemo(() => {
-    for (let i = 0; i < searchTerms.length; i++) {
-      let temp = Math.random();
-      temp = Math.ceil(temp * 10);
-      if (temp < searchTerms.length) {
-        let j = temp;
-        searchTerms[temp] = searchTerms[i];
-        searchTerms[i] = searchTerms[j];
-      }
-    }
-  }, []);
+  const [searchSuggestions, setSearchSuggestions] =
+    useState(searchRecommendation);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     toggleSearch();
@@ -135,17 +128,19 @@ export default function Searchbar() {
       </form>
       <div className="searchSuggestions">
         <motion.div variants={slideUp} className="searchTermContainer">
-          {searchTerms.map((item) => (
+          {searchSuggestions?.map((item) => (
             <motion.div
               key={item}
               className="searchItem"
               onClick={() => setFormInput(item)}
               variants={searchItemVariants}
+              title={item}
             >
-              {item}&nbsp;
+              <span className="searchItemInnerText">{item}</span>&nbsp;
               <TrendingUpIcon />
             </motion.div>
           ))}
+          {/* {searchSuggestions} */}
         </motion.div>
       </div>
     </motion.div>

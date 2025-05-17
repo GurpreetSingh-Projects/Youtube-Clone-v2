67d30cb61df938a7ff2bdaa64afbe72d272964d4
Gurpreet Singh <gpsingh02414@gmail.com>
2025-05-17 23:00:00 +0530

    Fix page going blank on refresh in inner pages #69

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,13 @@ import { Test } from "./components/Test.jsx";
 import Footer from "./components/Footer.jsx";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import "./styles.css";
+import Welcome from "./components/Welcome.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <Welcome />
     </Provider>
     {/* <Test /> */}
   </>

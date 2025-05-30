6470cbc9c2eaf318811559bd18b6cdffd122b86b
Gurpreet Singh <gpsingh02414@gmail.com>
2025-05-30 23:00:00 +0530

    Refactor Code for More Speed, Code reusage #59

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { configDotenv } from "dotenv";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), configDotenv()],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+      sourcemap: false,
+    },
+  },
 });

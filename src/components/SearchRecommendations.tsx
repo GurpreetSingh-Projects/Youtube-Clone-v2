52401819996ce81cd480c57efeaabe32d19752bb
Gurpreet Singh <gpsingh02414@gmail.com>
2025-07-22 23:00:00 +0530

    Refactor Codebase with Typescript usage #81, Refactor components to support Typescript #83, Implement Lazy Loading in all Components #84

--- src/components/SearchRecommendations.tsx
+++ src/components/SearchRecommendations.tsx
@@ -1,14 +1,14 @@
-import React from "react";
 import { Typewriter } from "react-simple-typewriter";
-
-const SearchRecommendations = (props) => {
+import { SearchRecommendationsProps } from "../utils/types";
+import React from "react";
+const SearchRecommendations: React.FC<SearchRecommendationsProps> = (props) => {
   return (
     <Typewriter
       words={props?.words}
       loop={true}
       cursor
       cursorStyle="|"
-      typeSpeed={70}
+      typeSpeed={50}
       deleteSpeed={20}
       delaySpeed={1000}
     />
